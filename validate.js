f98d68f80e1d23538812ed71655ec2db7f9fc517
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-08-23 18:16:26 +0200

    Add pw validation error message

--- validate.js
+++ validate.js
@@ -1,21 +1,25 @@
 var password = document.getElementById("password");
 var confirm_password = document.getElementById("confirm_password");
+var message = document.getElementById("pwvalidation");
 
 function validatePassword(){
   if(password.value != confirm_password.value) {
     confirm_password.setCustomValidity("Passwords Don't Match");
     password.style.borderColor = 'red';
     confirm_password.style.borderColor = 'red';
+    message.innerHTML = "*Password doesn't match.";
   } else if (password.value == confirm_password.value) {
     confirm_password.setCustomValidity('');
     password.style.borderColor = 'black';
     confirm_password.style.borderColor = 'black';
+    message.innerHTML = "";
   } else if (password.value == '' && confirm_password.value == ''){
     confirm_password.setCustomValidity('');
     password.style.borderColor = 'black';
     confirm_password.style.borderColor = 'black';
   } else {
     confirm_password.setCustomValidity('');
+    message.innerHTML = "";
   }
 }
 
